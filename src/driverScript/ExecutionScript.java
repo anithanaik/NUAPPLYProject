9143757f1eae96b279921dc62cb53f09532e29af
smishra <SantoshKumar.Mishra_IC@TrueValue.com>
2023-04-10 12:45:43 +0530

    handling pop up

--- src/driverScript/ExecutionScript.java
+++ src/driverScript/ExecutionScript.java
@@ -1,45 +1,90 @@
 package driverScript;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import utlility.CommonMethods;
-
 import pages.DegreeTypePage;
-
+import pages.*;
 
 public class ExecutionScript {
-	
-public static WebDriver driver; 	
-	
+
+	public static WebDriver driver;
+
 	public static WebDriver browserName(String bName) {
-		
+
 		if (bName.equalsIgnoreCase("Chrome")) {
 			driver = new ChromeDriver();
 		} else if (bName.equalsIgnoreCase("Firefox")) {
 			driver = new FirefoxDriver();
 		}
-		
+
 		return driver;
 	}
 
-public static void main(String[] args) {
-		
-		
-       browserName("Chrome");
-		
-		
-		 driver.get("https://nu-edu-develop4.go-vip.net/applyformio");
-		
-		 DegreeTypePage.Select_degreetype();
-		 try {
+	public static void main(String[] args) throws InterruptedException {
+
+		browserName("Chrome");
+
+		driver.get("https://nu-edu-develop4.go-vip.net/applyformio");
+		driver.manage().window().maximize();
+
+		DegreeTypePage.Select_degreetype();
+		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 DegreeTypePage.Degreetype_Nextbtn();
+		DegreeTypePage.Degreetype_Nextbtn();
+		handleFeedbackWindow();
+		Thread.sleep(2000);
+		DegreeTypePage.selectStudyArea();
+		DegreeTypePage.Nextbtn();
+		//handleFeedbackWindow();
 
+	}
+
+	public static void handleFeedbackWindow() throws InterruptedException {
+		Thread.sleep(8000);
+
+		List<WebElement> feedbackPopup = driver.findElements(By.xpath(
+				"//div[@class='QSIWebResponsive-creative-container-fade']/div/div[3]/button[contains(text(),'Provide Feedback')]"));
+		int i = feedbackPopup.size();
+		if (i > 0) {
+			driver.switchTo().activeElement();
+			driver.findElement(By.xpath(
+					"//div[@class='QSIWebResponsive-creative-container-fade']/div/div[3]/button[contains(text(),'No Thanks')]"))
+					.click();
+			waitForPageLoaded();
+		} else {
+			System.out.println("Provide feedback pop up not visible to user");
+			waitForPageLoaded();
+		}
+	}
+
+	public static void waitForPageLoaded() {
+		ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString()
+						.equals("complete");
+			}
+		};
+		try {
+			Thread.sleep(1000);
+			WebDriverWait wait = new WebDriverWait(driver, 30);
+			wait.until(expectation);
+		} catch (Throwable error) {
+			error.printStackTrace();
+		}
 	}
 
 }
--- src/pages/DegreeTypePage.java
+++ src/pages/DegreeTypePage.java
@@ -1,28 +1,67 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import driverScript.ExecutionScript;
 
 public class DegreeTypePage {
-	
-	public static void  Select_degreetype()
-    {
-WebDriverWait wait = new WebDriverWait(ExecutionScript.driver, 20);
- WebElement degreeradio= wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".form-check:nth-of-type(6) span")));
- degreeradio.click();
 
-    }
+	public static void Select_degreetype() {
+		WebDriverWait wait = new WebDriverWait(ExecutionScript.driver, 20);
+		WebElement degreeradio = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".form-check:nth-of-type(6) span")));
+		ScrollToElementAndClick(degreeradio);
 
+	}
 
-public static void  Degreetype_Nextbtn(){
-	  WebElement degreetypebtn = ExecutionScript.driver.findElement(By.xpath("//button[contains(.,'Next')]"));
-	  degreetypebtn.sendKeys(Keys.ENTER);
-}
+	public static void Degreetype_Nextbtn() throws InterruptedException {
+		Thread.sleep(2000);
+		WebElement degreetypebtn = ExecutionScript.driver.findElement(By.xpath("//button[contains(.,'Next')]"));
+		ScrollToElementAndClick(degreetypebtn);
 
+	}
+
+	public static void selectStudyArea() {
+		WebDriverWait wait = new WebDriverWait(ExecutionScript.driver, 20);
+		WebElement degreeradio = wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//span[contains(text(),'Science & Math')]/preceding-sibling::input")));
+		ScrollToElementAndClick(degreeradio);
+
+	}
+
+	public static void Nextbtn() throws InterruptedException {
+		Thread.sleep(2000);
+		WebElement degreetypebtn = ExecutionScript.driver.findElement(By.xpath("//button[contains(.,'Next')]"));
+		ScrollToElementAndClick(degreetypebtn);
+	}
+
+	public static void ScrollToElementAndClick(WebElement element) {
+		waitForElementToBeClickable(element);
+		JavascriptExecutor js = ((JavascriptExecutor) ExecutionScript.driver);
+		js.executeScript("arguments[0].scrollIntoView(true);", element);
+		js.executeScript("window.scrollBy(0, -250);", element);
+		js.executeScript("arguments[0].click();", element);
+
+	}
+
+	public static void waitForElementToBeClickable(WebElement element) {
+		try {
+			Wait<WebDriver> wait = new FluentWait<WebDriver>(ExecutionScript.driver).withTimeout(Duration.ofSeconds(50))
+					.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+			wait.until(ExpectedConditions.elementToBeClickable(element));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
